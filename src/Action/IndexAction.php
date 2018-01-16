8c33a206b4346f11208ab6a9727af9b93e30bfd7
Lucas Seinfeld <l.seinfeld@kiwi-suite.com>
2018-01-16 08:55:36 +0100

    automatically find built assets by name

--- src/Action/IndexAction.php
+++ src/Action/IndexAction.php
@@ -3,10 +3,12 @@ declare(strict_types=1);
 
 namespace KiwiSuite\Admin\Action;
 
+use FilesystemIterator;
 use Interop\Http\Server\MiddlewareInterface;
 use Interop\Http\Server\RequestHandlerInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use SplFileInfo;
 use Zend\Diactoros\Response\HtmlResponse;
 use Zend\Expressive\Plates\PlatesRenderer;
 
@@ -16,6 +18,7 @@ class IndexAction implements MiddlewareInterface
 
     public function __construct()
     {
+        // TODO: inject a TemplateRendererInterface
         $this->templateRenderer = new PlatesRenderer();
         $this->templateRenderer->addPath(__DIR__ . '/../../templates/admin', 'admin');
     }
@@ -25,21 +28,22 @@ class IndexAction implements MiddlewareInterface
         // TODO: read, cache and share contents of admin-frontend assets folder (scripts & css file names)
         // TODO: read project config for white labeling / deep merge with default config
 
-        $data = [
+        $data = array_merge($this->assetsPaths(), $this->config());
+
+        return new HtmlResponse($this->templateRenderer->render('admin::index', $data));
+    }
+
+    /**
+     * @return array
+     */
+    private function config()
+    {
+        return [
             'title'       => 'Kiwi CMF',
             'description' => 'Kiwi CMF Admin Area',
             'author'      => 'kiwi suite GmbH',
             'baseUrl'     => '/admin/',
             'assetsUrl'   => '/assets/admin/',
-            'styles'      => [
-                'styles.5c4b74b2975be54d4f58.bundle.css',
-            ],
-            'scripts'     => [
-                'inline.59af9aa1ba51e73d4f61.bundle.js',
-                'polyfills.83c2cf10225eb4ddb836.bundle.js',
-                'scripts.e6fa46938b14bf2a795f.bundle.js',
-                'main.747c6df7e803fbdae74c.bundle.js',
-            ],
             'config'      => [
                 'apiUrl'     => 'https://kiwi.test/api/',
                 'configPath' => 'config',
@@ -51,7 +55,60 @@ class IndexAction implements MiddlewareInterface
                 ],
             ],
         ];
+    }
 
-        return new HtmlResponse($this->templateRenderer->render('admin::index', $data));
+    /**
+     * automatically grab angular asset names from build directory
+     *
+     * @return array
+     */
+    private function assetsPaths()
+    {
+        $scripts = [
+            'inline'    => null,
+            'polyfills' => null,
+            'scripts'   => null,
+            'main'      => null,
+        ];
+
+        $styles = [
+            'styles' => null,
+        ];
+
+        /**
+         * admin and admin-frontend are siblings in a vendor folder
+         */
+        $embeddedPath = \realpath(__DIR__ . '/../../../admin-frontend/build');
+
+        /**
+         * admin-frontend is a local dependency of admin for development
+         */
+        $standalonePath = \realpath(__DIR__ . '/../../vendor/kiwi-suite/admin-frontend/build');
+
+        /**
+         * prefer embedded path
+         */
+        $path = \file_exists($embeddedPath) ? $embeddedPath : $standalonePath;
+
+        /**
+         * look up assets by name
+         */
+        $fileSystemIterator = new FilesystemIterator($path);
+        /** @var SplFileInfo $fileInfo */
+        foreach ($fileSystemIterator as $fileInfo) {
+            if ($fileInfo->getExtension() === 'js') {
+                $assetName = \explode('.', $fileInfo->getFilename())[0] ?? null;
+                if (\in_array($assetName, \array_keys($scripts))) {
+                    $scripts[$assetName] = $fileInfo->getFilename();
+                }
+            } else if ($fileInfo->getExtension() === 'css') {
+                $assetName = \explode('.', $fileInfo->getFilename())[0] ?? null;
+                if (\in_array($assetName, \array_keys($styles))) {
+                    $styles[$assetName] = $fileInfo->getFilename();
+                }
+            }
+        }
+
+        return ['scripts' => $scripts, 'styles' => $styles];
     }
 }
