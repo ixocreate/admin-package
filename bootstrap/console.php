4ed342e876306d17c88068d5ffb830fcd56fb566
Thomas Payer <1170097+thomaspayer@users.noreply.github.com>
2018-05-04 11:11:08 +0200

    cleanup

--- bootstrap/console.php
+++ bootstrap/console.php
@@ -2,8 +2,6 @@
 namespace KiwiSuite\Admin;
 
 /** @var ConsoleConfigurator $console */
-use KiwiSuite\ApplicationConsole\Command\CommandInterface;
 use KiwiSuite\ApplicationConsole\ConsoleConfigurator;
-use KiwiSuite\ServiceManager\ServiceManagerConfigurator;
 
 $console->addDirectory(__DIR__ . '/../src/Console', true);
