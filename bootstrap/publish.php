a9dfd7b2113a25e74ed2c962761f5620689f5cf3
Thomas Payer <1170097+thomaspayer@users.noreply.github.com>
2018-05-04 11:13:27 +0200

    added publish for migrations

---
+++ bootstrap/publish.php
@@ -0,0 +1,7 @@
+<?php
+namespace KiwiSuite\Admin;
+
+/** @var PublishConfigurator $publish */
+use KiwiSuite\Application\Publish\PublishConfigurator;
+
+$publish->add('migrations', __DIR__ . '/../resources/migrations');
---
+++ resources/migrations/Version20180221130347.php
@@ -0,0 +1,40 @@
+<?php declare(strict_types = 1);
+
+namespace KiwiMigration;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\DBAL\Types\Type;
+use KiwiSuite\Admin\Type\RoleType;
+use KiwiSuite\Admin\Type\StatusType;
+use KiwiSuite\CommonTypes\Entity\DateTimeType;
+use KiwiSuite\CommonTypes\Entity\EmailType;
+use KiwiSuite\CommonTypes\Entity\UuidType;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20180221130347 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        $table = $schema->createTable('admin_user');
+        $table->addColumn('id', UuidType::class);
+        $table->addColumn('email', EmailType::class);
+        $table->addColumn('password', Type::STRING)->setLength(255);
+        $table->addColumn('hash', UuidType::class);
+        $table->addColumn('role', RoleType::class)->setLength(255);
+        $table->addColumn('avatar', Type::TEXT);
+        $table->addColumn("status", StatusType::class, ['default' => 'active']);
+        $table->addColumn('createdAt', DateTimeType::class);
+        $table->addColumn('lastLoginAt', DateTimeType::class)->setNotnull(false);
+
+        $table->setPrimaryKey(["id"]);
+        $table->addUniqueIndex(["email"]);
+    }
+
+    public function down(Schema $schema)
+    {
+        $schema->dropTable("admin_user");
+    }
+}
