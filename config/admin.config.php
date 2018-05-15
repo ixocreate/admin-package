187ffc2450068ffbf065424959c5e0a9bd95bca5
Lucas Seinfeld <l.seinfeld@kiwi-suite.com>
2018-05-15 20:46:12 +0200

    config cleanup

--- config/admin.config.php
+++ config/admin.config.php
@@ -5,15 +5,15 @@ return [
      * project specific settings (white label)
      */
     'project' => [
-        'author' => 'kiwi suite GmbH',
-        'copyright' => '2018',
+        'author'      => 'kiwi suite GmbH',
+        'copyright'   => '2018',
         'description' => 'Kiwi Admin',
-        'name' => 'Kiwi',
-        'poweredBy' => true,
-        'mediaUrl' => '',
-        'logo' => '/assets/admin/assets/img/kiwi-logo.svg',
-        'icon' => '/assets/admin/assets/img/kiwi-icon.svg',
-        'background' => '/assets/admin/assets/img/kiwi-icon.svg',
+        'name'        => 'Kiwi',
+        'poweredBy'   => true,
+        'mediaUrl'    => '',
+        'logo'        => '/assets/admin/assets/img/kiwi-logo.svg',
+        'icon'        => '/assets/admin/assets/img/kiwi-icon.svg',
+        'background'  => '/assets/admin/assets/img/kiwi-icon.svg',
     ],
     /**
      * navigation
@@ -46,5 +46,5 @@ return [
         //         'admin'
         //     ]
         // ],
-    ]
+    ],
 ];
