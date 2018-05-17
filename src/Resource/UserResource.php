8acebc79cea033a50ba31d605361320179451759
Lucas Seinfeld <l.seinfeld@kiwi-suite.com>
2018-05-17 11:13:31 +0200

    user schema

--- src/Resource/UserResource.php
+++ src/Resource/UserResource.php
@@ -2,10 +2,10 @@
 /**
  * kiwi-suite/admin (https://github.com/kiwi-suite/admin)
  *
- * @package kiwi-suite/admin
- * @see https://github.com/kiwi-suite/admin
+ * @package   kiwi-suite/admin
+ * @see       https://github.com/kiwi-suite/admin
  * @copyright Copyright (c) 2010 - 2018 kiwi suite GmbH
- * @license MIT License
+ * @license   MIT License
  */
 
 declare(strict_types=1);
@@ -37,5 +37,13 @@ final class UserResource implements ResourceInterface
     public function createMessage(): string
     {
         return CreateUserMessage::class;
+    }
+
+    public function schema(): array
+    {
+        return [
+            'name'       => 'User',
+            'namePlural' => 'Users',
+        ];
     }
 }
