9ddea8cabe64d97ba871c61d0f2c0ed20559efdf
Lucas Seinfeld <l.seinfeld@ixolit.com>
2019-01-07 18:55:41 +0100

    fix cs

--- resources/migrations/Version20180917123947.php
+++ resources/migrations/Version20180917123947.php
@@ -1,4 +1,5 @@
-<?php declare(strict_types=1);
+<?php
+declare(strict_types=1);
 
 namespace IxocreateMigration;
 
@@ -8,14 +9,14 @@ use Ixocreate\CommonTypes\Entity\DateTimeType;
 
 final class Version20180917123947 extends AbstractMigration
 {
-    public function up(Schema $schema) : void
+    public function up(Schema $schema): void
     {
         $table = $schema->getTable('admin_user');
         $table->addColumn('updatedAt', DateTimeType::serviceName());
         $table->addColumn('deletedAt', DateTimeType::serviceName())->setNotnull(false);
     }
 
-    public function down(Schema $schema) : void
+    public function down(Schema $schema): void
     {
         $table = $schema->getTable('admin_user');
         $table->dropColumn('updatedAt');
--- resources/migrations/Version20181130114739.php
+++ resources/migrations/Version20181130114739.php
@@ -1,4 +1,5 @@
-<?php declare(strict_types=1);
+<?php
+declare(strict_types=1);
 
 namespace IxocreateMigration;
 
@@ -8,14 +9,14 @@ use Ixocreate\CommonTypes\Entity\SchemaType;
 
 final class Version20181130114739 extends AbstractMigration
 {
-    public function up(Schema $schema) : void
+    public function up(Schema $schema): void
     {
         $table = $schema->getTable('admin_user');
-        $table->addColumn('additionalFields',SchemaType::serviceName())->setNotnull(false);
+        $table->addColumn('additionalFields', SchemaType::serviceName())->setNotnull(false);
 
     }
 
-    public function down(Schema $schema) : void
+    public function down(Schema $schema): void
     {
         $table = $schema->getTable('admin_user');
         $table->dropColumn('additionalFields');
