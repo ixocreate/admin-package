582fbd73a750acd12310163c154600a242d46f21
Thomas Payer <1170097+thomaspayer@users.noreply.github.com>
2019-03-11 16:19:46 +0100

    added logout

---
+++ src/Action/Auth/LogoutAction.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * @link https://github.com/ixocreate
+ * @copyright IXOCREATE GmbH
+ * @license MIT License
+ */
+
+declare(strict_types=1);
+
+namespace Ixocreate\Admin\Action\Auth;
+
+use Firebase\JWT\JWT;
+use Ixocreate\Admin\Config\AdminConfig;
+use Ixocreate\Admin\Entity\SessionData;
+use Ixocreate\Admin\Entity\User;
+use Ixocreate\Admin\Repository\UserRepository;
+use Ixocreate\Admin\Router\AdminRouter;
+use Ixocreate\Admin\Session\SessionCookie;
+use Ixocreate\Application\ApplicationConfig;
+use Ixocreate\Template\TemplateResponse;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use Ramsey\Uuid\Uuid;
+use Zend\Diactoros\Response\RedirectResponse;
+
+final class LogoutAction implements MiddlewareInterface
+{
+    /**
+     * @var AdminConfig
+     */
+    protected $adminConfig;
+
+    /**
+     * @var AdminRouter
+     */
+    private $adminRouter;
+
+    /**
+     * IndexAction constructor.
+     * @param AdminConfig $adminConfig
+     * @param AdminRouter $adminRouter
+     * @param ApplicationConfig $applicationConfig
+     * @param UserRepository $userRepository
+     */
+    public function __construct(AdminConfig $adminConfig, AdminRouter $adminRouter, ApplicationConfig $applicationConfig, UserRepository $userRepository)
+    {
+        $this->adminConfig = $adminConfig;
+        $this->adminRouter = $adminRouter;
+    }
+
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        $response = new RedirectResponse($this->adminRouter->generateUri('admin.login'));
+
+        $sessionData = new SessionData([
+            'xsrfToken' => Uuid::uuid4()->toString(),
+        ]);
+        $sessionCookie = new SessionCookie();
+        $response = $sessionCookie->createSessionCookie($request, $response, $this->adminConfig, $sessionData);
+        $response = $sessionCookie->createXsrfCookie($request, $response, $sessionData);
+
+        return $response;
+    }
+}
