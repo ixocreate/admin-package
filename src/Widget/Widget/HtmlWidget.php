1a910f5c51728b0115314fec520ea990ce65a480
Thomas Payer <1170097+thomaspayer@users.noreply.github.com>
2020-03-10 13:49:17 +0100

    added HtmlWidget

---
+++ src/Widget/Widget/HtmlWidget.php
@@ -0,0 +1,113 @@
+<?php
+/**
+ * @link https://github.com/ixocreate
+ * @copyright IXOLIT GmbH
+ * @license MIT License
+ */
+
+declare(strict_types=1);
+
+namespace Ixocreate\Admin\Widget\Widget;
+
+use Ixocreate\Admin\Widget\WidgetInterface;
+
+final class HtmlWidget implements WidgetInterface
+{
+    /**
+     * @var int
+     */
+    private $size = self::SIZE_LARGE;
+
+    /**
+     * @var int
+     */
+    private $priority = 100;
+
+    /**
+     * @var array
+     */
+    private $data = [
+        'html' => ''
+    ];
+
+    /**
+     * @return int
+     */
+    public function size(): int
+    {
+        return $this->size;
+    }
+
+    /**
+     * @return int
+     */
+    public function priority(): int
+    {
+        return $this->priority;
+    }
+
+    /**
+     * @return string
+     */
+    public function type(): string
+    {
+        return 'html';
+    }
+
+    /**
+     * @param int $size
+     * @return HtmlWidget
+     */
+    public function withSize(int $size): HtmlWidget
+    {
+        $widget = clone $this;
+        $widget->size = $size;
+
+        return $widget;
+    }
+
+    /**
+     * @param int $priority
+     * @return HtmlWidget
+     */
+    public function withPriority(int $priority): HtmlWidget
+    {
+        $widget = clone $this;
+        $widget->priority = $priority;
+
+        return $widget;
+    }
+
+    public function withHtml(string $html): HtmlWidget
+    {
+        $widget = clone $this;
+        $widget->data['html'] = $html;
+
+        return $widget;
+    }
+
+
+    /**
+     * @return array
+     */
+    public function data(): array
+    {
+        return $this->data;
+    }
+
+    /**
+     * Specify data which should be serialized to JSON
+     * @see https://php.net/manual/en/jsonserializable.jsonserialize.php
+     * @return mixed data which can be serialized by <b>json_encode</b>,
+     * which is a value of any type other than a resource
+     * @since 5.4.0
+     */
+    public function jsonSerialize()
+    {
+        return [
+            'size' => $this->size(),
+            'type' => $this->type(),
+            'data' => $this->data(),
+        ];
+    }
+}
